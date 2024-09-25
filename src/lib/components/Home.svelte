2877e33eba69387d6cd17160e39e3d3d2d0b0b24
mik135 <michaelsavior1@gmail.com>
2024-09-25 17:39:48 +0100

    Add Download Icon

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -57,7 +57,7 @@
 
 							<div class="dropdown static">
 								<div tabindex="0" role="button" class="btn bg-gradient-watch text-white border-none font-bold shadow-md w-full lg:w-96 uppercase col-span-2">
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="h-6 w-6" fill="#fff"><path d="M64 64l0 288 512 0 0-288L64 64zM0 64C0 28.7 28.7 0 64 0L576 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64L64 416c-35.3 0-64-28.7-64-64L0 64zM128 448l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-384 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg>
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-6 h-6" fill="#fff"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
 									{language == "filipino" ? "PANOORIN" : "Download Rhapsody App"}</div>
 								<ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-65 text-lg p-4 mt-2 shadow-2xl  ">
 									<li><a href="https://web.lwappstore.com/share/appId-32181354074e5cf63319371178894acd">
