d5b1d96f2975ae47c8cc048610be237c4d8d9a25
mik135 <michaelsavior1@gmail.com>
2024-09-29 03:37:05 +0100

    add redirect

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -1,49 +1,10 @@
 <script>
-  import { goto } from '$app/navigation';
-
-  function redirectToAdmin() {
-    goto('https://reachoutadmin.vercel.app');
-  }
+  
 </script>
 
 
 <main class="bg-white h-[100vh]">
-    <div class="overflow-x-auto">
-        <table class="table table-lg">
-          <!-- head -->
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Role</th>
-            </tr>
-          </thead>
-          <tbody>
-            <!-- row 1 -->
-            <tr class="bg-base-200">
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-            <!-- row 2 -->
-            <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
-            </tr>
-            <!-- row 3 -->
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
-            </tr>
-          </tbody>
-        </table>
-    </div>
+    
 </main>
 
 <style>
